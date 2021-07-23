9cfbdf30d39367ca53874c64283820f01afc2f39
Adarsha <vatraiadarsh@gmail.com>
2021-07-23 13:17:37 +1000

    using @WebServlet()

---
+++ src/main/java/com/ada/web/controller/ColorController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ada.web.controller;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author vatra
+ */
+public class ColorController extends HttpServlet{
+    
+}
